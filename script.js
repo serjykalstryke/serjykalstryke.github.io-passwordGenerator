13dd94c34b10747144a2c2634f19cba2d09fcfb7
David Stinnett <davidstinnett@icloud.com>
2020-08-31 21:20:34 -0400

    updated project with some of the javascript functions that will be used to eventually generate password

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Generator functions -- character code list found at https://www.net-comber.com/charset.html
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+console.log(getRandomSymbol())
